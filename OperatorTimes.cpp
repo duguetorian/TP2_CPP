150853c210432aecef30d3cf7823e31de95d6104
duguetorian <87642767+duguetorian@users.noreply.github.com>
2022-10-05 09:52:56 +0200

    Correction Times (#1)
    Co-authored-by: Prince Cyrille <cyrille.prince@student-cs.fr>

--- OperatorTimes.cpp
+++ OperatorTimes.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <functional>
+
+#include "OperatorTimes.h"
+#include "Operator.h"
+
 int OperatorTimes::operator()(int left, int right) const
     {
         return left * right;
